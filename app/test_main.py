acaef632d01c3de4a12da1579f1871fd7a203475
Ambra <trank13f@gmail.com>
2026-01-18 18:42:35 +0100

    change test structure, use parametrize to optimise testing

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,18 @@
+import pytest
+
+
 from app.main import get_human_age
 
-def test_should_return_list_with_0():
-    cat_age = 13
-    dog_age = 12
-    assert get_human_age(cat_age, dog_age) == [0, 0]
 
-
-def test_should_return_list_with_1():
-    pass
-
-
-def test_should_return_list_with_more_then_2():
-    pass
-
-
-def test_should_return_list_with_different_values():
-    pass
+@pytest.mark.parametrize(
+    'cat_age, dog_age,  result',
+    [
+        pytest.param(13, 12, [0, 0], id="animal years less then 15 years"),
+        pytest.param(15, 16, [1, 1], id="animal years equell or more 15 but less 24"),
+        pytest.param(24, 24, [2, 2], id="animal years equell or more 24 but less 32"),
+        pytest.param(28, 28, [3, 3], id="animal years equell or more 32"),
+        pytest.param(32, 16, [4, 1], id="animal years equell different value"),
+    ]
+)
+def test_should_return_list_with_0(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
