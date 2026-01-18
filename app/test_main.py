f786c4d9366a36fd6b6432ecfe49c48a164ddcf4
Ambra <trank13f@gmail.com>
2026-01-18 18:27:00 +0100

    write the body for future tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,18 @@
 from app.main import get_human_age
 
-def test_animals_yonger_then_15_years():
+def test_should_return_list_with_0():
     cat_age = 13
     dog_age = 12
     assert get_human_age(cat_age, dog_age) == [0, 0]
+
+
+def test_should_return_list_with_1():
+    pass
+
+
+def test_should_return_list_with_more_then_2():
+    pass
+
+
+def test_should_return_list_with_different_values():
+    pass
